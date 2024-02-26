398f43ef96f61be9a0354e4c4b063f13cb86caf6
Fasrad <millerchaz@gmail.com>
2024-02-25 17:47:18 -0700

    workin'

--- avr555plus.c
+++ avr555plus.c
@@ -1,7 +1,6 @@
 /*-------|---------|---------|---------|---------|---------|---------|---------|
 avr555plus.c	
 
-A universal AVR firmware
 
 by chaz miller 
 
@@ -16,29 +15,30 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ******************************************************************/
 
 #include <avr/io.h>
+#define boil_setpoint 0x980    //legacy
+#define hyst 2    //legacy. 1/10th deg. C per count at 12 bits; optional
 
-#define boil_setpoint 0x980    //adjust to your system
-#define hyst 2    //about 1/10th deg. C per count at 12 bits; optional
-
-void delay(uint16_t);
-void blink (uint8_t);
-void adc_init(void);
-uint16_t adc_read(uint8_t);
+void delay(uint16_t); // generic delay function 
+void blink (uint8_t); // blinks an LED for diagnostic
+void adc_init(void);  // Inits ADC at 12 bits oversampled 
+/* adc_read takes PORTC bitmask. Returns 12 bit value. Blocking; not tested to be 
+interrupt-safe. Depends on clock setups. ADC*/
+uint16_t adc_read(uint8_t); 
 
 uint16_t probe_ad;  
 uint16_t set_ad;  
 
 int main(){
-    //8 bit Timer 0 is used by delay().
+    //8 bit Timer 0 is used by delay(). PWM on PD6 and PD5. 
     TCCR0A = 0;                //standard timer mode (page 103)
     TCCR0B = 2;                //fcpu / 1
-    //16-bit Timer 1 used as output PWM on OC1B PB2 (Arduino pin 10) p.115
+    //16-bit Timer 1 used as output PWM on OC1B PB2 p.115
     //noninverting phase correct, CTC-PWM hybrid mode p135 
     TCCR1A = (1<<COM1B1)|(1<<WGM11)|(1<<WGM10); 
     TCCR1B = (1<<WGM13)|(1<<CS12);                 //  clk/256
     //TCCR1B = (1<<WGM13)|(1<<CS11)|(1<<CS10);       //  clk/64
     //TCCR1B = (1<<WGM13)|(1<<CS10);                 //  clk/1
-    OCR1A = 0x0FF0;             //sets pwm TOP 
+    OCR1A = 0x0FF0;             //sets pwm TOP value
     OCR1B = 0;
     PORTB = 0xFF;
     DDRB = 0b000100100;        //LED on PB5; OC1B is PB2
@@ -46,35 +46,14 @@ int main(){
     /****************************************
     *****main loop***************************
     ****************************************/
-    blink(3);
+    blink(4);
     // this is where we do hardware detection.
-
-    /* This is setup stuff we used in brewtroller
-	if(PINB&1<<3){                      //if switch is not 'WFO'
-	    PINB|=1<<5;                     //turn on contactor
-	    if(PINB&1<<4){                  //if switch is not 'boil'
-		probe_ad = adc_read(0); 
-		if(probe_ad < 0x0EFF){       //continue iff probe detected 
-		    set_ad = adc_read(1);                
-		    if(set_ad > (probe_ad + hyst)){   //thermostat block
-			OCR1B = adc_read(2) & 0xFF00;
-		    }else if(set_ad < (probe_ad - hyst)){
-			OCR1B = 0x0000;
-		    }
-		}else{                       //no probe; no boil override
-		    OCR1B = adc_read(2) & 0xFF00;
-		}
-	    }else{                           //'boil' switch is on
-		OCR1B = boil_setpoint;
-	    }
-	}else{
-	    OCR1B = OCR1A;                 //WFO switch is on
-	}
-    */
 
     //this is where we call functions in other source files like program0, program1 etc., possibly with a switch statement?
 
     for(;;){  
+    blink(2);
+    delay(500);
     } //infty
 }//main
 
@@ -83,7 +62,7 @@ void adc_init(void){
     ADCSRA = (1<<ADEN)|(1<<ADPS2); //62kHz @ 1MHz clock, page 264
     ADMUX |= (1<<REFS0);           //Avcc (5v)
     //ADMUX |= (1<<ADLAR);           //left align for 8-bit operation
-    ADCSRA |= (1<<ADEN); 
+    ADCSRA |= (1<<ADEN);  // redundant?? Some you have to write twice...
     //ADCSRA |= (1<<ADSC);  
 }
 
